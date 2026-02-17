f9ae9b5c93a94d1a41a0a74f05202522872c51e9
ShanuDevCodes <shanu.dev.codes@gmail.com>
2026-02-17 13:10:13 +0530

    -bug fix

--- composeApp/src/iosMain/kotlin/com/kito/core/platform/ConnectivityObserver.ios.kt
+++ composeApp/src/iosMain/kotlin/com/kito/core/platform/ConnectivityObserver.ios.kt
@@ -2,8 +2,31 @@ package com.kito.core.platform
 
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import platform.Network.nw_path_get_status
+import platform.Network.nw_path_monitor_create
+import platform.Network.nw_path_monitor_set_queue
+import platform.Network.nw_path_monitor_set_update_handler
+import platform.Network.nw_path_monitor_start
+import platform.Network.nw_path_monitor_t
+import platform.Network.nw_path_status_satisfied
+import platform.darwin.dispatch_get_global_queue
+import platform.darwin.dispatch_queue_t
 
 actual class ConnectivityObserver {
-    actual val isOnline: StateFlow<Boolean> = MutableStateFlow(true)
-    // TODO: Implement using NWPathMonitor
+    private val _isOnline = MutableStateFlow(true)
+    actual val isOnline: StateFlow<Boolean> = _isOnline.asStateFlow()
+
+    init {
+        val monitor: nw_path_monitor_t = nw_path_monitor_create()
+        val queue: dispatch_queue_t = dispatch_get_global_queue(0.toLong(), 0.toULong())
+
+        nw_path_monitor_set_update_handler(monitor) { path ->
+            val status = nw_path_get_status(path)
+            _isOnline.value = status == nw_path_status_satisfied
+        }
+
+        nw_path_monitor_set_queue(monitor, queue)
+        nw_path_monitor_start(monitor)
+    }
 }
